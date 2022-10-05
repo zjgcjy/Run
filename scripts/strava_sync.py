9480ba50292113ae21401da318772ea45e0d9aef
zjgcjy <zjgcjy@gmail.com>
2022-10-05 22:07:34 +0800

    modify strava_to_tcx with clcd

--- scripts/strava_sync.py
+++ scripts/strava_sync.py
@@ -23,14 +23,15 @@ def strava_to_tcx(act_l):
             with open(fname, 'wb') as f:
                 f.write(ret.content)
 
-def run_strava_sync(client_id, client_secret, refresh_token):
+def run_strava_sync(client_id, client_secret, refresh_token, tcx=False):
     generator = Generator(SQL_FILE)
     generator.set_strava_config(client_id, client_secret, refresh_token)
     # if you want to refresh data change False to True
     generator.sync(False)
 
     activities_list = generator.load()
-    strava_to_tcx(activities_list)
+    if tcx:
+        strava_to_tcx(activities_list)
     
     with open(JSON_FILE, "w") as f:
         json.dump(activities_list, f)
@@ -41,5 +42,6 @@ if __name__ == "__main__":
     parser.add_argument("client_id", help="strava client id")
     parser.add_argument("client_secret", help="strava client secret")
     parser.add_argument("refresh_token", help="strava refresh token")
+    parser.add_argument("--with-tcx", dest="with_tcx", action="store_true", help="export tcx files")
     options = parser.parse_args()
-    run_strava_sync(options.client_id, options.client_secret, options.refresh_token)
+    run_strava_sync(options.client_id, options.client_secret, options.refresh_token, options.with_tcx)
